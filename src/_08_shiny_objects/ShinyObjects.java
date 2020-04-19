720003ba6cbdc65d2d849070de54d0cde026c762
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-04-19 14:30:27 -0700

    YEEEET
    SHINYYYY

--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,13 +8,18 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+String objects = JOptionPane.showInputDialog(null, "how much shiny objects do you want?");
 		// 3. Play the sound that many times
+int object = Integer.parseInt(objects);
+for (int i = 0; i < object; i++) {
+	playMisterZee();	
+}
 
 		// 1. Call the method below
 
